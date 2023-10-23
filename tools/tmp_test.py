2aaf60ee79c819f0498e4b376d7f8819a6755252
zhou <zbjustdoit2021@163.com>
2023-10-23 20:49:42 +0800

    test

--- tools/tmp_test.py
+++ tools/tmp_test.py
@@ -1,6 +1,24 @@
-a = [1,2,4,3,7,6]
+import asyncio
 
-for i in a:
-    if i == 6:
-        a.append(9)
-    print(i)
+async def func1():
+    print("我是func1")
+    await asyncio.sleep(3)
+    print("func1结束")
+
+async def func2():
+    print("我是func2")
+    await asyncio.sleep(2)
+    print("func2结束")
+
+async def func3():
+    print("我是func3")
+    await asyncio.sleep(1)
+    print("func3结束")
+
+
+if __name__ == '__main__':
+    f1 = func1()
+    f2 = func2()
+    f3 = func3()
+    tasks = [f1,f2,f3]
+    asyncio.run(asyncio.wait(tasks))
