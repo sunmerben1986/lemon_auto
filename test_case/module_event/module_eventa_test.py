fe49c45f18b8434fd4cf7c4a8b35d1626130fa1d
zhou <zbjustdoit2021@163.com>
2024-03-19 23:13:30 +0800

    20240319 重新修改用例执行方式

--- test_case/module_event/module_eventa_test.py
+++ test_case/module_event/module_eventa_test.py
@@ -1,113 +1,92 @@
-import asyncio
 import json
-import sys
-import os
 import time
 import random
 import unittest
 from tools.websocket_utils import connect
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
-print(sys.path)
 from page.module_obj import Module_obj as mo
 from page.document_conf import module_event_after as mp
 from page.document_conf import fe_module_eventa as fmp
 from tools.database import db, record, module_eventa_tmp
 
 
-
 class module_eventa_test(unittest.IsolatedAsyncioTestCase):
-#模型创建事件，创建后触发
+    timestamp = 0
+    async def asyncSetUp(self):
+        if not hasattr(type(self), '_is_setup'):
+            type(self)._is_setup = True
+            self.websocket = await connect()
+            message_list = []
+            self.timestamp = int(time.time())
+            moo = mo()
+            message_list.extend([moo.create_page(mp.page_uuid), moo.init_event(mp.data_list)])
+            for message in message_list:
+                await self.websocket.send(json.dumps(message))
+                while True:
+                    response = await self.websocket.recv()
+                    res = await self.handle_message(response)
+                    if res is None:
+                        break
+                    elif res != 200 and len(res) == 32:            
+                        inline_create = moo.create_inline(mp.data_list, res)
+                        inline_data, item =await self.set_inline_data(self.timestamp, [mp.text1, mp.text2])
+                        inline_data = moo.add_inline_data(mp.data_list, res, inline_data)
+                        message_list.append(inline_create)
+                        message_list.append(inline_data)
+                        break
+                    elif res == 200:
+                        pass
+                    else:
+                        break
+            await self.websocket.close()
+
+
+    #模型创建事件，创建后触发
     async def test_inline_sm(self):
-        websocket = await connect()
-        message_list = []
-        moo = mo()
-        message_list.extend([moo.create_page(mp.page_uuid), moo.init_event(mp.data_list)])
-        for message in message_list:
-            await websocket.send(json.dumps(message))
-            while True:
-                response = await websocket.recv()
-                res = await self.handle_message(response)
-                if res is None:
-                    break
-                elif res != 200 and len(res) == 32:            
-                    inline_create = moo.create_inline(mp.data_list, res)
-                    message_list.append(inline_create)
-                elif res == 200:
-                    pass
-                else:
-                    break
-        await websocket.close()
-        response = await self.isSucess("create", int(time.time()))
-        if response:
-            return True
-        else:
-            return False
+        response = await self.isSucess("create", self.timestamp)
+        self.assertIsNotNone(response)
         
     #模型保存事件，保存后触发
     async def test_inline_sm_add(self):
-        websocket = await connect()
-        message_list = []
-        moo = mo()
-        message_list.extend([moo.create_page(mp.page_uuid), moo.init_event(mp.data_list)])
-        for message in message_list:
-            await websocket.send(json.dumps(message))
-            while True:
-                response = await websocket.recv()
-                res = await self.handle_message(response)
-                if res is None:
-                    break
-                elif res != 200 and len(res) == 32:            
-                    inline_create = moo.create_inline(mp.data_list, res)
-                    inline_data, item = self.set_inline_data(res, [mp.text1, mp.text2])
-                    inline_data = moo.add_inline_data(mp.data_list, res, inline_data)
-                    message_list.append(inline_create)
-                    message_list.append(inline_data)
-                elif res == 200:
-                    pass
-                else:
-                    break
-        await websocket.close()
-        response = await self.isSucess("save", item)
-        if response:
-            return True
-        else:
-            return False
+        response = await self.isSucess("normal_save", self.timestamp)
+        self.assertIsNotNone(response)
         
-    #模型删除事件，删除后触发
-    async def test_delete_inline_data(self):
-        websocket = await connect()
-        message_list = []
-        moo = mo()
-        pk_list = self.get_pk_list()
-        message_list.extend([moo.create_page(mp.page_uuid), moo.init_event(mp.data_list), 
-                            moo.delete_event_confirm(mp.data_list, pk_list), moo.delete_event(mp.data_list), moo.refresh_event(mp.page_uuid)])
-        for message in message_list:
-            await websocket.send(json.dumps(message))
-        await websocket.close()
-        time_stamp = int(time.time())
-        response = await self.isSucess("delete", time_stamp)
-        if response:
-            return True
-        else:
-            return False
+    # #模型删除事件，删除后触发
+    # async def test_delete_inline_data(self):
+    #     websocket = await connect()
+    #     message_list = []
+    #     moo = mo()
+    #     pk_list = await self.get_pk_list()
+    #     message_list.extend([moo.create_page(mp.page_uuid), moo.init_event(mp.data_list), 
+    #                         moo.delete_event_confirm(mp.data_list, pk_list), moo.delete_event(mp.data_list), moo.refresh_event(mp.page_uuid)])
+    #     for message in message_list:
+    #         print(f"message_delete:{message}")
+    #         await websocket.send(json.dumps(message))
+    #     await websocket.close()
+    #     time_stamp = int(time.time())
+    #     response = await self.isSucess("delete", time_stamp)
+    #     if response:
+    #         return True
+    #     else:
+    #         return False
         
-    #模型取消事件，取消前触发
-    async def test_delete_inline_data(self):
-        websocket = await connect()
-        message_list = []
-        moo = mo()
-        pk_dict = self.get_pk_dict()
-        message_list.extend([moo.create_page(mp.page_uuid), moo.init_event(mp.data_list), 
-                            moo.edit_event(mp.page_uuid, mp.data_list, pk_dict), moo.any_event(fmp.page_uuid, "event_edit"), moo.refresh_event(mp.page_uuid)])
-        for message in message_list:
-            await websocket.send(json.dumps(message))
-        await websocket.close()
-        time_stamp = int(time.time())
-        response = await self.isSucess("delete", time_stamp)
-        if response:
-            return True
-        else:
-            return False
+    # #模型取消事件，取消前触发
+    # async def test_cancel_inline_data(self):
+    #     websocket = await connect()
+    #     message_list = []
+    #     moo = mo()
+    #     pk_dict = await self.get_pk_dict()
+    #     message_list.extend([moo.create_page(mp.page_uuid), moo.init_event(mp.data_list), 
+    #                         moo.edit_event(mp.page_uuid, mp.data_list, pk_dict), moo.any_event(fmp.page_uuid, "event_edit"), moo.refresh_event(mp.page_uuid)])
+    #     for message in message_list:
+    #         print(f"message_cancel:{message}")
+    #         await websocket.send(json.dumps(message))
+    #     await websocket.close()
+    #     time_stamp = int(time.time())
+    #     response = await self.isSucess("delete", time_stamp)
+    #     if response:
+    #         return True
+    #     else:
+    #         return False
 
     async def isSucess(self,tag,item):
         db.connect()
@@ -116,6 +95,7 @@ class module_eventa_test(unittest.IsolatedAsyncioTestCase):
                                                 record.记录 == tag, record.前后 == "after").first()
         elif tag == "save":
             record_data = record.select().where(record.编号 == item, record.记录 == tag, record.前后 == "after").first()
+        db.close()
         return record_data
 
     async def get_pk_list(self):
@@ -134,12 +114,11 @@ class module_eventa_test(unittest.IsolatedAsyncioTestCase):
         db.close()
         return pk_dict
 
-    async def set_inline_data(self, inline_uuid, field_uuid_list):
+    async def set_inline_data(self, time_stamp, field_uuid_list):
         digits = "0123456789"
         ascii_letters = "abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         str_list = [random.choice(digits +ascii_letters) for i in range(5)]
         random_str = ''.join(str_list)
-        time_stamp = int(time.time())
         inline_data = {field_uuid_list[0]:time_stamp, field_uuid_list[1]:random_str}
         return inline_data, time_stamp
 
@@ -161,4 +140,5 @@ if __name__ == "__main__":
     runner = unittest.TextTestRunner()
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(module_eventa_test))
+    # suite.addTest(module_eventa_test("test_inline_sm_add"))
     runner.run(suite)
--- tools/websocket_utils.py
+++ tools/websocket_utils.py
@@ -1,17 +1,16 @@
-import asyncio
 import websockets
 import json
 
+
 async def connect():
     host = "tlemon.lemonstudio.tech:8443/8f646bbd87e25fecb72e128eb4f98c49test/api/runtime/v1"
-    parameter1 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3V1aWQiOiI1MWRiNWE4Nzg0YzA1ZmQwYjY0NGQ0NzA3M2Q2MTFkYSIsImV4cCI6MTczMTA3NjUxMH0.09upeK9YBcfEAFnQs-aZlAIzNZKtbSVWdFZk4NpFZdk"
+    parameter1 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3V1aWQiOiI1MWRiNWE4Nzg0YzA1ZmQwYjY0NGQ0NzA3M2Q2MTFkYSIsImV4cCI6MTczNjE0MjA4OH0.831Pfrd4dKuYsIS5hAIOQLY_XldORH4TIB5YI3t0kDk"
     parameter2 = "b858071bb83c5aa0bd9a40ce09572d9e"
     parameter3 = 0
-    parameter4 = 1699778379198
+    parameter4 = 1710583854079
     parameter5 = "0681ebe843dd56ea957270b9fcf2b21c"
     
     url = f"wss://{host}/ws?token={parameter1}&tenant_uuid={parameter2}&SessionId={parameter4}&device_type={parameter3}"
-
     websocket = await websockets.connect(url)
     return websocket
 
